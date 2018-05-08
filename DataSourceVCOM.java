b296c0f7ee6975e29734a452b22f11816654d393
guizhigang <guizhigang168@126.com>
2018-05-08 14:02:15 +0800

    add file to dev branch

---
+++ DataSourceVCOM.java
@@ -0,0 +1,16 @@
+package com.youpu.datasource;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * <br> 威科姆切库
+ * @author C152
+ *
+ */
+public class DataSourceVCOM {
+
+	 public static  Map<String,String> vcomdb = new HashMap<String,String>();
+	 static{
+		 vcomdb.put("tmskw.czbanbantong.com", "");	//用户中心系统测试版
+	 }
+}
